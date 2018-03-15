af2237725ac081d0ff225798f1d399fc291a1695
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2018-03-15 23:32:51 +0100

    working install

--- DBConnection/DependencyDBSQL/DependencyDB_Install.sql
+++ DBConnection/DependencyDBSQL/DependencyDB_Install.sql
@@ -6,6 +6,17 @@ CREATE PROCEDURE [DependencyDB].[Install]
 	@TBL_ProcedureParameters dbo.TYPE_ParametersType READONLY,
 	@V_ValidFor INT
 AS
+
+--DECLARE
+--	@V_SubscriberString NVARCHAR(200) = 'SubscriberString',
+--	@V_SubscriptionHash INT = '1234564',
+--	@V_ProcedureSchemaName SYSNAME = 'NotificationBroker',
+--	@V_ProcedureName SYSNAME = 'A_JobParts_Select',
+--	@TBL_ProcedureParameters dbo.SpParametersType,
+--	@V_ValidFor INT = 30
+
+--INSERT INTO @TBL_ProcedureParameters (PName, Ptype, PValue)
+--VALUES ('@order_id', 'int', -1) , ('@job_id', 'int', -1)
 BEGIN
 
 	DECLARE @V_MainName SYSNAME = '{0}' ;
@@ -86,13 +97,14 @@ BEGIN
 	SELECT @V_startPoz = MIN ( TBL_StartPoz.C_StartPoz )
 		FROM @TBL_StartPoz AS TBL_StartPoz
 		WHERE TBL_StartPoz.C_StartPoz > 7;
-	SET @V_ProcedureText = SUBSTRING(@V_ProcedureText , @V_startPoz ,LEN( @V_ProcedureText ) - @V_startPoz + 1) ;
+	SET @V_ProcedureText = @V_ProcedureParametersDeclaration + '
+		' + SUBSTRING(@V_ProcedureText , @V_startPoz ,LEN( @V_ProcedureText ) - @V_startPoz + 1) ;
 	
 	-- get result table definition
 	DECLARE @V_ResultTableDefinition NVARCHAR(max)
 	SET @V_ResultTableDefinition =
-		(SELECT ',' + TBL_ResultDefinition.name + ' ' + TBL_ResultDefinition.system_type_name --+   CHAR(10) 
-		FROM [sys].[dm_exec_describe_first_result_set] ( @V_ProcedureText, @V_ProcedureParametersList, 0) AS TBL_ResultDefinition
+		(SELECT ',' + quotename( TBL_ResultDefinition.name ) + ' ' + TBL_ResultDefinition.system_type_name --+   CHAR(10) 
+		FROM [sys].[dm_exec_describe_first_result_set] ( @V_ProcedureText, null, 0) AS TBL_ResultDefinition
 		FOR XML PATH('')) ;
 	SET @V_ResultTableDefinition = '
 		DECLARE @TBL_ResultTable TABLE (
@@ -101,47 +113,57 @@ BEGIN
 		+ '
 		) ;
 	' ;
-
-	-- get tables used in procedure
+	
+	-- get tables used in procedure must be written with schema in procedure
 	DECLARE @TBL_ReferencedTables TABLE (
-		[name] SYSNAME
+		[SchemaName] SYSNAME,
+		[TableName] SYSNAME
 	) ;
 	INSERT INTO @TBL_ReferencedTables
 		SELECT 
-				quotename( ReferencedEntities.referenced_schema_name ) + '.'  + quotename( ReferencedEntities.referenced_entity_name ) ,*
+				ReferencedEntities.referenced_schema_name AS [SchemaName],
+				ReferencedEntities.referenced_entity_name AS [TableName]
 			FROM sys.dm_sql_referenced_entities ( quotename( @V_ProcedureSchemaName ) + '.' + quotename( @V_ProcedureName ) , 'OBJECT' ) AS ReferencedEntities
 			WHERE ReferencedEntities.referenced_minor_id = 0
 				AND ReferencedEntities.referenced_entity_name IS NOT NULL
 				AND ReferencedEntities.referenced_schema_name IS NOT NULL
+			GROUP BY ReferencedEntities.referenced_schema_name, 
+				ReferencedEntities.referenced_entity_name ;
 
 	-- for each affected table
+	DECLARE @V_TrigerName SYSNAME ;
+	DECLARE @V_TrigerBody NVARCHAR(max)
+	DECLARE @V_ReferencedQuotedTable NVARCHAR(256) ;
+	DECLARE @V_ReferencedNonQuotedTable NVARCHAR(256) ;
+	DECLARE @V_ReferencedSchema SYSNAME ;
 	DECLARE @V_ReferencedTable SYSNAME ;
 	DECLARE ReferencedTablesCursor CURSOR FOR
-		SELECT [name]
+		SELECT [SchemaName], [TableName]
 			FROM @TBL_ReferencedTables ;
 	OPEN ReferencedTablesCursor ;
-	FETCH NEXT FROM ReferencedTablesCursor INTO @V_ReferencedTable ;
+	FETCH NEXT FROM ReferencedTablesCursor INTO @V_ReferencedSchema, @V_ReferencedTable ;
 	WHILE @@FETCH_STATUS = 0
 		BEGIN
 		
-			DECLARE @V_TrigerName NVARCHAR(128) ;
-			SET @V_TrigerName = @V_MainName + '_' + REPLACE(@V_ReferencedTable, '.', '_') + '_' + @V_SubscriptionHash ;
+			SET @V_TrigerName =  @V_MainName + '_' + @V_ReferencedSchema + '_' + @V_ReferencedTable + '_' + CAST( @V_SubscriptionHash AS NVARCHAR(200))  ;
+			SET @V_ReferencedQuotedTable = quotename( @V_ReferencedSchema ) + '.' + quotename( @V_ReferencedTable )
+			SET @V_ReferencedNonQuotedTable = @V_ReferencedSchema + '.' + @V_ReferencedTable
 			
-			-- Trigger statement.
-			DECLARE @V_TrigerBody NVARCHAR(max)
+			-- Trigger statement
 			SET @V_TrigerBody = '
-				ON ' + quotename( @V_ReferencedTable ) + ' 
+				ON ' + @V_ReferencedQuotedTable + ' 
 				WITH EXECUTE AS ''' + USER_NAME() + '''
 				AFTER INSERT, UPDATE, DELETE
 				AS 
 				BEGIN
 					SET NOCOUNT ON; 
 
-					DECLARE @V_Message NVARCHAR(MAX)
-					DECLARE @V_MessageInserted NVARCHAR(MAX)
-					DECLARE @V_MessageParameters NVARCHAR(MAX)
-					DECLARE @V_MessageDeleted NVARCHAR(MAX)
-					DECLARE @V_retvalOUT NVARCHAR(MAX)
+					DECLARE @V_Message NVARCHAR(MAX) ;
+					DECLARE @V_MessageInserted NVARCHAR(MAX) ;
+					DECLARE @V_MessageParameters NVARCHAR(MAX) ; 
+					DECLARE @V_MessageDeleted NVARCHAR(MAX) ;
+					DECLARE @V_Retval NVARCHAR(MAX) ;
+					DECLARE @V_Cmd NVARCHAR(MAX) ;
 
 					' + @V_ResultTableDefinition + '
 									
@@ -152,39 +174,56 @@ BEGIN
 							FROM INSERTED
 					)
 						BEGIN
-							
+							SET @V_Cmd = 
+								''' + REPLACE( 
+										REPLACE( 
+											REPLACE( @V_ProcedureText, @V_ReferencedQuotedTable, 'INSERTED') 
+										, @V_ReferencedNonQuotedTable , 'INSERTED') 
+									, '''', '''''' ) + ''' ;
+
 
 							INSERT INTO @TBL_ResultTable
-							EXEC sp_executesql 
+							EXEC sp_executesql @V_Cmd ;
+
+							SET @V_MessageInserted = 
+								(SELECT *
+									FROM @TBL_ResultTable
+									FOR XML PATH(''row''), ROOT(''inserted'')) ;
+
+							DELETE FROM @TBL_ResultTable ;
 						END
-						SET @V_retvalOUT = (
-						-- start inner procedure
-						' + REPLACE( @V_ProcedureText, @V_ReferencedTable, 'INSERTED') + '
-						-- end inner procedure
-						)
-					IF (@V_retvalOUT IS NOT NULL)
-						SET @V_MessageInserted = ''<inserted>'' + @V_retvalOUT + ''</inserted>''
 
 					-- deleted rows
 					IF EXISTS (
 						SELECT * 
 							FROM DELETED
 					)
-						SET @V_retvalOUT = (
-						-- start inner procedure
-						' + REPLACE( @V_ProcedureText, @V_ReferencedTable, 'DELETED') + '
-						-- end inner procedure
-						)
-					IF (@V_retvalOUT IS NOT NULL)
-						SET @V_MessageDeleted = ''<deleted>'' + @V_retvalOUT + ''</deleted>''
+						BEGIN
+							SET @V_Cmd = 
+								''' + REPLACE( 
+										REPLACE( 
+											REPLACE( @V_ProcedureText, @V_ReferencedQuotedTable, 'DELETED') 
+										, @V_ReferencedNonQuotedTable , 'DELETED') 
+									, '''', '''''' ) + ''' ;
 
-					-- IF no changes return
+
+							INSERT INTO @TBL_ResultTable
+							EXEC sp_executesql @V_Cmd ;
+
+							SET @V_MessageDeleted = 
+								(SELECT *
+									FROM @TBL_ResultTable
+									FOR XML PATH(''row''), ROOT(''deleted'')) ;
+
+							DELETE FROM @TBL_ResultTable ;
+						END
+
 					IF @V_MessageInserted IS NOT NULL 
 						OR @V_MessageDeleted IS NOT NULL
 						BEGIN
 							-- create final message
 							SET @V_Message = ''<notification>''
-							SET @V_Message = @V_Message + ''' + @V_ProcedureParametersXlm + N'''
+							SET @V_Message = @V_Message + ''' + @V_ProcedureParametersXlm + '''
 							IF @V_MessageInserted IS NOT NULL 
 								SET @V_Message = @V_Message + @V_MessageInserted
 							IF @V_MessageDeleted IS NOT NULL 
@@ -208,12 +247,14 @@ BEGIN
 						END
 				END 
 			' ;
+			
 			BEGIN TRANSACTION 
 				-- lock whole table
 				SET @V_Cmd = '
+					DECLARE @V_Dummy int
 					SELECT TOP 1 
-							1 
-						FROM ' + quotename( @V_ReferencedTable ) + '
+							@V_Dummy = 1 
+						FROM ' + @V_ReferencedQuotedTable + '
 						WITH (UPDLOCK, TABLOCKX, HOLDLOCK) 
 					' ;
 				EXEC sp_executesql @V_Cmd ;
@@ -225,23 +266,23 @@ BEGIN
 				)
 					BEGIN
 						SET @V_Cmd = '
-							CREATE TRIGGER [' + @V_TrigerName + N'] 
+							CREATE TRIGGER ' + quotename( @V_TrigerName ) + ' 
 							' + @V_TrigerBody ;
 						EXEC sp_executesql @V_Cmd ;
 					END
 				ELSE
 					BEGIN
 						SET @V_Cmd = N'
-							ALTER TRIGGER [' + @V_TrigerName + N'] 
+							ALTER TRIGGER ' + quotename( @V_TrigerName ) + ' 
 							' + @V_TrigerBody ;
 						EXEC sp_executesql @V_Cmd ;
 					END
+				
 			COMMIT TRANSACTION;
-			FETCH NEXT FROM ReferencedTablesCursor INTO @V_ReferencedTable ;
+			FETCH NEXT FROM ReferencedTablesCursor INTO @V_ReferencedSchema, @V_ReferencedTable ;
 		END
 	CLOSE ReferencedTablesCursor ;
 	DEALLOCATE ReferencedTablesCursor ;
 END
-
 
 
